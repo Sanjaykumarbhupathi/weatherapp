a9549374ceb2dd6f7353b3d825e269b4e32934ad
Bhupathi Sanjay Kumar <91123208+Sanjaykumarbhupathi@users.noreply.github.com>
2024-04-15 16:42:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-// require('dotenv').config();
-// const cors = require('cors');
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const userRoutes = require('./routes/userRoutes');
-
-// const app = express();
-// const PORT = process.env.PORT || 4000;
-
-// app.use(cors());
-
-
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-// .then(() => console.log('Connected to MongoDB'))
-// .catch(err => console.error('Failed to connect to MongoDB', err));
-
-// app.use(express.json());
-// app.use('/api/users', userRoutes);
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
@@ -33,11 +7,9 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// CORS Configuration
 app.use(cors());
 const MONGODB_URI="mongodb+srv://weather2024:weather2024@weather2024.jo5iubu.mongodb.net/?retryWrites=true&w=majority&appName=weather2024"
 
-// MongoDB Connection
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -49,13 +21,10 @@ mongoose.connect(MONGODB_URI, {
     console.error('Failed to connect to MongoDB', err);
 });
 
-// Parse JSON request body
 app.use(express.json());
 
-// API Routes
 app.use('/api/users', userRoutes);
 
-// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
